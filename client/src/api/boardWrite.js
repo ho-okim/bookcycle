ea58249b7451db2098c49ae6d1c94bdc64d876b6
ho-okim <peanutdolphin00@gmail.com>
2024-04-24 12:43:48 +0900

    Feat: Add fileupload api in boardWrite.js

--- client/src/api/boardWrite.js
+++ client/src/api/boardWrite.js
@@ -1,13 +1,28 @@
 import axios from '../lib/axios.js';
 
-async function boardWrite(title, content) {
+export async function boardWrite(title, content) {
     const res = await axios.post('/boardwrite', { title, content });
     
     if (res.statusText != "OK") {
         throw new Error("post fails");
     } 
     const body = res.data;
+    body.message = 'success'
+    console.log(body.message)
     return body;
 }
 
-export default boardWrite;
+export async function fileupload(formData) {
+  const res = await axios.post('/fileupload', formData, {
+    headers: {
+      "Content-Type": "multipart/form-data"
+    }
+  })
+  
+  if (res.statusText != "OK") {
+      throw new Error("file upload fails");
+  } 
+  const body = res.data;
+  console.log('fileupload: ', res.statusText)
+  return res.statusText;
+}
