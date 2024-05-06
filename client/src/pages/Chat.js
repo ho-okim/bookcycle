b08b2a4d5fd9bb962d11325e3e8987377f6f07e1
ho-okim <peanutdolphin00@gmail.com>
2024-05-06 22:04:23 +0900

    Chore: Change css, Add userId useState in Chat.js

--- client/src/pages/Chat.js
+++ client/src/pages/Chat.js
@@ -30,6 +30,7 @@ function Chat() {
   const [readOrNot, setReadOrNot] = useState([])
   // 현재 우측에 열린 chatRoom에 넣을 상대방 닉네임, 프로필, 상품 정보
   const [nickname, setNickname] = useState(null)
+  const [userId, setUserId] = useState(null)
   const [profile, setProfile] = useState(null)
   const [product, setProduct] = useState(null)
   // 소켓 통신을 통해 전송되는 메세지 한 개
@@ -65,6 +66,7 @@ function Chat() {
     setChatroomIdx(chatroom_id)
     let chatroom = chatRoom[i]
     setNickname(chatroom.user_nickname)
+    setUserId(chatroom.user_id)
     setProfile(chatroom.profile_image)
     setActive(i)
   }
@@ -154,101 +156,103 @@ function Chat() {
   // }
 
   return (
-    <Container className={`chattingSec sec`}>
-      <h3 className={`${styles.title}`}>대화 목록</h3>
-      {
-        chatRoom.length != 0 ?
-        <div className={`inner d-flex justify-content-between`}>
-        <div className={`${styles.box}`}>
-          <div className={`${styles.chattingList}`}>
-            {
-              chatRoom.map((el, i)=>{
-                return(
-                  <div className={`${styles.chatUser}` + (i == active ? ` ${styles.active}` : "")} key={i} value={i} onClick={(e) => handleChatroomClick(e, el.chatroom_id, i)} >
-                    <ChatUser el={el} readOrNot={readOrNot[i]}/>
-                  </div>
-                )
-              })
-            }
-          </div>
-        </div>
-        <div className={`${styles.box}`}>
-          {
-            chatroomIdx == null ?
-            <div className={`${styles.emptyChat} d-flex flex-column justify-content-center align-items-center regular`}>
-              <SendFill className={`${styles.send}`}/>
-              <span>좌측의 채팅방을 클릭하여</span>
-              <span>대화를 시작하세요</span>
-            </div>
-            :
-            <div className={`${styles.chattingRoom}`}>
-              <div className={`${styles.profileWrap} d-flex justify-content-between`}>
-                <Link style={{ textDecoration: 'none' }}>
-                  <div className='d-flex'>
-                    <div className={`d-flex justify-content-center align-items-center ${styles.profileImgWrap}`}>
-                      {
-                        profile == '' ? <Person className={`${styles.profileIcon}`}/>
-                        : <img src={process.env.PUBLIC_URL + `/img/${profile}`} className={`${styles.profileImg}`}/>
-                      }
-                    </div>
-                    <div className={`${styles.nickname} d-flex align-items-center`}>
-                      <p>{nickname}</p>
-                    </div>
-                  </div>
-                </Link>
-                <div className={`d-flex align-items-center`}>
-                  <div><Link className={`${styles.report}`} style={{ textDecoration: 'none' }}>신고하기</Link></div>
-                </div>
-              </div>
+    <Container className={`chattingSec p-0`}>
+      <div className='inner'>
+        <h3 className={`${styles.title}`}>대화 목록</h3>
+        {
+          chatRoom.length != 0 ?
+          <div className={`inner d-flex justify-content-between`}>
+          <div className={`${styles.box}`}>
+            <div className={`${styles.chattingList}`}>
               {
-                product &&
-                <>
-                  <div className={`${styles.productWrap} d-flex justify-content-between`}>
-                  <Link style={{ textDecoration: 'none' }}>
-                    <div className={`d-flex`}>
-                      <div className={`${styles.productImgWrap} d-flex justify-content-center align-items-center`}>
-                        <img src={process.env.PUBLIC_URL + `/img/${product.filename}`} alt='' className={`${styles.profileImg}`}/>
+                chatRoom.map((el, i)=>{
+                  return(
+                    <div className={`${styles.chatUser}` + (i == active ? ` ${styles.active}` : "")} key={i} value={i} onClick={(e) => handleChatroomClick(e, el.chatroom_id, i)} >
+                      <ChatUser el={el} readOrNot={readOrNot[i]}/>
+                    </div>
+                  )
+                })
+              }
+            </div>
+          </div>
+          <div className={`${styles.box} ${styles.chatBoxWrap}`}>
+            {
+              chatroomIdx == null ?
+              <div className={`${styles.emptyChat} d-flex flex-column justify-content-center align-items-center regular`}>
+                <SendFill className={`${styles.send}`}/>
+                <span>좌측의 채팅방을 클릭하여</span>
+                <span>대화를 시작하세요</span>
+              </div>
+              :
+              <div className={`${styles.chattingRoom}`}>
+                <div className={`${styles.profileWrap} d-flex justify-content-between`}>
+                  <Link style={{ textDecoration: 'none' }} to={`/user/${userId}`}>
+                    <div className='d-flex'>
+                      <div className={`d-flex justify-content-center align-items-center ${styles.profileImgWrap}`}>
+                        {
+                          profile == '' ? <Person className={`${styles.profileIcon}`}/>
+                          : <img src={process.env.PUBLIC_URL + `/img/${profile}`} className={`${styles.profileImg}`}/>
+                        }
                       </div>
-                      <div className={`${styles.productContent} d-flex justify-content-center flex-column`}>
-                        <div className={`${styles.productName}`}>{product.product_name}</div>
-                        <div className={`${styles.price} regular`}>{product.price.toLocaleString()}원</div>
+                      <div className={`${styles.nickname} d-flex align-items-center`}>
+                        <p>{nickname}</p>
                       </div>
                     </div>
                   </Link>
-                    <div className={`d-flex align-items-center`}>
-                      <Button variant="outline-danger">후기 작성</Button>
-                    </div>
+                  <div className={`d-flex align-items-center`}>
+                    <div><Link className={`${styles.report}`}>신고하기</Link></div>
                   </div>
-                </>
-              }
-              <div className={`${styles.chatting}`}>
-                <div>
-                  {
-                    msgs && msgs.map((el)=>{
-                      return(
-                        <ChatMessage key={el.id} el={el} nickname={nickname}/>
-                      )
-                    })
-                  }
                 </div>
-                <div className={`${styles.chatForm} d-flex`}>
-                  <textarea className={`${styles.chatText} regular`} ref={textRef} onInput={handleResizeHeight}></textarea>
-                  <span className={`${styles.chatBtn} d-flex align-items-center`} onClick={handleMsgClick}><SendFill/></span>
+                {
+                  product &&
+                  <>
+                    <div className={`${styles.productWrap} d-flex justify-content-between`}>
+                    <Link style={{ textDecoration: 'none' }}>
+                      <div className={`d-flex`}>
+                        <div className={`${styles.productImgWrap} d-flex justify-content-center align-items-center`}>
+                          <img src={process.env.PUBLIC_URL + `/img/${product.filename}`} alt='' className={`${styles.profileImg}`}/>
+                        </div>
+                        <div className={`${styles.productContent} d-flex justify-content-center flex-column`}>
+                          <div className={`${styles.productName}`}>{product.product_name}</div>
+                          <div className={`${styles.price} regular`}>{product.price.toLocaleString()}원</div>
+                        </div>
+                      </div>
+                    </Link>
+                      <div className={`d-flex align-items-center`}>
+                        <Button variant="outline-danger">후기 작성</Button>
+                      </div>
+                    </div>
+                  </>
+                }
+                <div className={`${styles.chatting}`}>
+                  <div>
+                    {
+                      msgs && msgs.map((el)=>{
+                        return(
+                          <ChatMessage key={el.id} el={el} nickname={nickname}/>
+                        )
+                      })
+                    }
+                  </div>
+                  <div className={`${styles.chatForm} d-flex`}>
+                    <textarea className={`${styles.chatText} regular`} ref={textRef} onInput={handleResizeHeight}></textarea>
+                    <span className={`${styles.chatBtn} d-flex align-items-center`} onClick={handleMsgClick}><SendFill/></span>
+                  </div>
                 </div>
               </div>
-            </div>
-          }
+            }
+          </div>
         </div>
-      </div>
-      :
-      <div className={`${styles.empty} d-flex justify-content-center align-items-center text-center medium`}>
-        <div>
-          <EmojiTear className={`${styles.tear}`}></EmojiTear>
-          <p>아직 생성된 채팅방이 없습니다.</p>
-          <p>거래를 시작하여 대화를 나눠보세요!</p>
+        :
+        <div className={`${styles.empty} d-flex justify-content-center align-items-center text-center medium`}>
+          <div>
+            <EmojiTear className={`${styles.tear}`}></EmojiTear>
+            <p>아직 생성된 채팅방이 없습니다.</p>
+            <p>거래를 시작하여 대화를 나눠보세요!</p>
+          </div>
         </div>
+        }
       </div>
-      }
     </Container>
   );
 }
