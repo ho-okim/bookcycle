1f4cb5fefeddc23a3bfe2560d87c03d492987454
ho-okim <peanutdolphin00@gmail.com>
2024-05-10 16:29:49 +0900

    Feat: Add dateProcessing year and time

--- client/src/lib/dateProcessing.js
+++ client/src/lib/dateProcessing.js
@@ -3,4 +3,21 @@ function dateProcessing(date) {
   return date ? new Date(date).toLocaleDateString("ko-KR", options).replaceAll('.', '.') : "";
 }
 
+export function dateProcessingYT(date){
+  // 데이터의 createdAt이 date 객체로 들어오는 게 아니라 string으로 들어옴에 주의
+  let newDate = new Date(date)
+  let year = newDate.toLocaleString("ko-kr", {dateStyle:'long'})
+  let time = newDate.toLocaleString("ko-kr").slice(12, -3)
+
+  return {year, time}
+}
+
+export function dateProcessingYear(date){
+  // 데이터의 createdAt이 date 객체로 들어오는 게 아니라 string으로 들어옴에 주의
+  let newDate = new Date(date)
+  let year = newDate.toLocaleString("ko-kr", {dateStyle:'long'})
+
+  return year
+}
+
 export default dateProcessing;
