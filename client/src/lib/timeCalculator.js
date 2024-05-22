6c903188e51f69b4f504cfa46bc580a1df69d2aa
ho-okim <peanutdolphin00@gmail.com>
2024-05-22 17:36:00 +0900

    Feat: Modify dateprocessing, Add timecalc for chat

--- client/src/lib/timeCalculator.js
+++ client/src/lib/timeCalculator.js
@@ -1,3 +1,5 @@
+import { dateProcessingYT, dateProcessingYear } from "./dateProcessing";
+
 function timeCalculator(date) {
     const now = new Date();
     const past = new Date(date);
@@ -19,6 +21,48 @@ function timeCalculator(date) {
         const days = Math.floor(diffInSeconds / secondsInDay);
         return `${days}일 전`;
     }
+}
+
+// 채팅방에서 사용할 timeCaculator
+export function chatTimeCalculator(date) {
+  const now = new Date();
+  const past = new Date(date);
+  const diffInSeconds = Math.floor((now - past) / 1000);
+  const time = dateProcessingYT(date)?.time
+  const year = date ? dateProcessingYear(date) : null
+
+  const secondsInMinute = 60;
+  const secondsInHour = 3600;
+  const secondsInDay = 86400;
+
+  if (diffInSeconds < secondsInDay) {
+      return time;
+  } else {
+      return year;
+  }
+}
+
+// date 값의 타입이 string일 때로 간주
+export function isSameDate(date, prevDate){
+  if(!date || !prevDate){
+
+    // date나 prevDate 중 하나라도 undefined면 false return
+    return false;
+
+  } else {
+    // string으로 들어온 데이터 Date 형식으로 변환
+    const newDate = new Date(date);
+    const newPrevDate = new Date(prevDate);
+
+    const day = newDate.toLocaleDateString('en-US', {year: 'numeric', month: '2-digit', day: '2-digit'})
+    const prevDay = newPrevDate.toLocaleDateString('en-US', {year: 'numeric', month: '2-digit', day: '2-digit'})
+
+    if(day == prevDay){
+      return true;
+    }
+
+    return false;
+  }
 }
 
 export default timeCalculator;
